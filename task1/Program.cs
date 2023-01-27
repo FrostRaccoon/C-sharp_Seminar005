95b4904f0142c5e28c75c3cfbd5bff4505c3a693
EnotWhyNot <danik_maltsev@mail.ru>
2023-01-27 22:00:27 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,55 @@
+﻿// 
+
+
+
+int[] arr = GetRandomArray(10);
+PrintArray(arr);
+
+Console.WriteLine(GetSumMoreZero(arr));
+Console.WriteLine(GetSumLessZero(arr));
+
+int GetSumMoreZero(int[] array)
+{
+    int result = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] >= 0)
+        {
+            result += arr[i];
+        }
+    }
+    return result;
+}
+
+int GetSumLessZero(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0)
+        {
+            result += arr[i];
+        }
+    }
+    return result;
+}
+
+int[] GetRandomArray(int length)
+{
+    int[] result = new int[length];
+
+    for (int i = 0; i < length; i++) 
+    {
+        result[i] = new Random().Next(-20, 21);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
