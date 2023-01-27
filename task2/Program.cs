46dac3ecb1c4d04838d824df66c449c3978e10dc
EnotWhyNot <danik_maltsev@mail.ru>
2023-01-27 22:00:34 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,36 @@
+﻿//
+
+
+
+int[] arr = GetRandomArray(10);
+PrintArray(arr);
+PrintArray(ReplaceNum(arr));
+
+int[] ReplaceNum(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+    return array;
+}
+
+int[] GetRandomArray(int length)
+{
+    int[] result = new int[length];
+
+    for (int i = 0; i < length; i++) 
+    {
+        result[i] = new Random().Next(-20, 21);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
